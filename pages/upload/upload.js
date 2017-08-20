77ce0f5042bbc6d8b05f63f01a60ca1c964f78a6
liminjun <v-leeli@hotmail.com>
2017-08-20 21:32:02 +0800

    feat:add admin page for add store info.

---
+++ pages/upload/upload.js
@@ -0,0 +1,137 @@
+var app = getApp();
+Page({
+  data: {
+    categories: [],
+    categoryIndex: 0,
+    files: [],
+    filePaths: [],
+    maxNumber: 0,//上传图片计算
+  },
+  onLoad(options) {
+    this.fetchCategoryList()
+  },
+  chooseImage: function (e) {
+
+
+    let params = {};
+    params.formData = {
+
+    }
+    var that = this;
+    that.data.maxNumber++;
+    wx.chooseImage({
+      sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
+      sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
+      success: function (res) {
+        // 返回选定照片的本地文件路径列表，tempFilePath可以作为img标签的src属性显示图
+        that.setData({
+          files: that.data.files.concat(res.tempFilePaths),
+          maxNumber: that.data.maxNumber
+        });
+        //提交到后台
+        for (var i = 0; i < res.tempFilePaths.length; i++) {
+          params.filePath = res.tempFilePaths[i];
+          wx.BaaS.uploadFile(params).then((res) => {
+            // success. 服务器成功响应
+            /* 注: 只要是服务器有响应的情况都会进入 success, 即便是 4xx，5xx 都会进入
+              这是微信的处理方式与 BaaS 服务(器)无关
+              如果上传成功则会返回资源远程地址
+              如果上传失败则会返回失败信息
+            */
+            console.log(res);
+            var imagePath = JSON.parse(res.data).path;
+
+            that.data.filePaths.push(imagePath);
+            // 目前开发者工具上传文件功能有 bug, 请务必使用设备进行操作，同时打开 console 观察结果
+          }, (err) => {
+            // 微信自身系统级别错误
+            console.dir(err);
+          })
+        }
+
+      }
+    })
+  },
+  previewImage: function (e) {
+    wx.previewImage({
+      current: e.currentTarget.id, // 当前显示图片的http链接
+      urls: this.data.files // 需要预览的图片http链接列表
+    })
+  },
+
+
+  //bindCategoryChange
+  bindCategoryChange: function (e) {
+    console.log('picker category 发生选择改变，携带值为', e.detail.value);
+
+    this.setData({
+      categoryIndex: e.detail.value
+    })
+  },
+  //获取帖子分类列表
+  fetchCategoryList() {
+    let that = this
+    let tableID = 822;
+    let objects = {
+      tableID
+    }
+
+    wx.BaaS.getRecordList(objects).then((res) => {
+      that.setData({
+        categories: res.data.objects
+      })
+    }, (err) => {
+      console.dir(err)
+    });
+  },
+
+  //添加帖子
+  submitPost: function (e) {
+
+
+
+
+    let that = this;
+    let tableID = 821;
+
+
+    let imagePaths = that.data.filePaths[0];
+    let categoryId = that.data.categories[that.data.categoryIndex].id;
+    let categroyName = that.data.categories[that.data.categoryIndex].category_name;
+    let name = e.detail.value.name;
+    let address = e.detail.value.address;
+    let rate =3;// parseFloat(e.detail.value.rate);
+    let phone ="Coming Soon.";// e.detail.value.phone;
+    
+    let data = {
+      name: name,
+      address: address,
+      rate: rate,
+      phone: phone,
+      cover: imagePaths,
+      category_id: categoryId,
+      category_name:categroyName
+    }
+    let objects = {
+      tableID,
+      data
+    }
+
+    // 创建一个数据项
+    wx.BaaS.createRecord(objects).then((res) => {
+
+      wx.showToast({
+        title: '发布成功',
+        icon: 'success',
+        duration: 2000
+      });
+
+
+    }, (err) => {
+      console.log(err)
+    })
+  },
+
+
+
+})
